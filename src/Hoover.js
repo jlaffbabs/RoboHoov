341398ce59110fd4532e693265b34db7bad6a85a
Jacques <jacques.laffy@gmail.com>
2019-07-09 16:25:03 +0100

    Hoover can now move correctly

--- src/Hoover.js
+++ src/Hoover.js
@@ -3,4 +3,18 @@ class Hoover {
     this.x = hooverPosition[0];
     this.y = hooverPosition[1];
   }
+
+  move(directions) {
+    for (var i = 0; i < directions.length; i++) {
+      if (directions[i] === "N") {
+        this.y += 1
+      } else if (directions[i] === "S") {
+        this.y -= 1
+      } else if (directions[i] === "E") {
+        this.x += 1
+      } else if (directions[i] === "W") {
+        this.x -=1
+      }
+    }
+  }
 }
