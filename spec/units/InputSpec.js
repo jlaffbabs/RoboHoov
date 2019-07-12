e8c1f2f30efaea04408743f764a36fefda6207b0
Jacques <jacques.laffy@gmail.com>
2019-07-12 12:40:33 +0100

    Can now separate inputs into individual variables

--- spec/units/InputSpec.js
+++ spec/units/InputSpec.js
@@ -10,7 +10,16 @@ describe('Input', function () {
   });
 
   it("should transform text file into array", function () {
-    input.parse('./input.txt')
+    input.parse('./input.txt');
     expect(input.data).toEqual(exampleData)
+  });
+
+  it("should separate the data into the correct sub-array", function () {
+    input.parse('./input.txt');
+    input.separate();
+    expect(input.roomSize).toEqual([5, 5])
+    expect(input.dirtSpots).toEqual([[1, 0], [2, 2], [2, 3]])
+    expect(input.hooverPosition).toEqual([1, 2])
+    expect(input.directions).toEqual("NNESEESWNWW")
   });
 });
--- src/Input.js
+++ src/Input.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 class Input {
   constructor() {
     this.data = []
+    this.roomSize = []
+    this.hooverPosition = []
+    this.directions = ""
+    this.dirtSpots = []
   }
 
   parse(data) {
@@ -10,6 +14,17 @@ class Input {
     var stringInfo = info.split('\n');
     this.data.push(...stringInfo)
     this.data.pop()
+  }
+
+  separate() {
+    this.roomSize = this.data[0].split(' ').map(Number)
+    this.hooverPosition = this.data[1].split(' ').map(Number)
+    this.directions = this.data.pop()
+    var dirt = this.data.splice(2)
+    for (var i = 0; i < dirt.length; i++) {
+      dirt[i] = dirt[i].split(' ').map(Number)
+    }
+    this.dirtSpots = dirt
   }
 }
 
